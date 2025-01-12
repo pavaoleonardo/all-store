f6a507718a0492e58f9cfed578b365edc243f067
leonardo <pavaoleonardo@hotmail.com>
2025-01-12 17:54:17 +0100

    error handling w zod

--- lib/validators.ts
+++ lib/validators.ts
@@ -31,12 +31,14 @@ export const signInFormSchema = z.object({
 });
 
 // Schema for sigin up users in
-export const signUpFormSchema = z.object({
-  name: z.string().min(3, 'Name must be at least 3 characters long'),
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters long'), 
-  confirmPassword: z.string().min(6, 'Confirm password '),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: 'Passwords do not match', 
-  path: 'confirmPassword',
-} );
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, 'Name must be at least 3 characters long'),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    confirmPassword: z.string().min(6, 'Confirm password '),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
