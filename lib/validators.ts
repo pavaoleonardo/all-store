df1cf2b98edb3eacc64353f7e71cf4ddcd1a26cd
leonardo <pavaoleonardo@hotmail.com>
2025-01-14 23:03:43 +0100

    add to cart configuration-2

--- lib/validators.ts
+++ lib/validators.ts
@@ -44,14 +44,14 @@ export const signUpFormSchema = z
   });
 
   // Cart schemas
-  export const cartItemSchema = z
-  .object({
+  export const cartItemSchema = z.object({
     productId: z.string().min(1, 'Product is required '),
     name: z.string().min(1, 'Name is required '),
     slug: z.string().min(1, 'Slug is required '),
-    qty: z.number().int().nonnegative( 'Quantity must be a positive number '),
+    qty: z.number().int().nonnegative('Quantity must be a positive number '),
+    image: z.string().min(1, 'Image is required'),
     price: currency,
-  })
+  });
 
   export const insertCartSchema = z
   .object({
