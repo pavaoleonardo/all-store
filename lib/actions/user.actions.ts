91e28694a4f8836cfd775a6bcc7e05267813711c
leonardo <pavaoleonardo@hotmail.com>
2025-02-17 19:51:37 +0100

    adding payment methods page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  paymentMethodSchema,
 } from '../validators';
 import { auth, signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
@@ -11,6 +12,8 @@ import { hash } from '../encrypt';
 import { prisma } from '@/db/prisma';
 import { formatError } from '../utils';
 import { ShippingAddress } from '@/types';
+import { z } from 'zod';
+import { error } from 'console';
 
 //Sigin in the user with credentials
 export async function signInWithCredentials(
@@ -103,6 +106,28 @@ export async function updateUserAddress(data: ShippingAddress) {
 
     return { success: true, message: 'User address updated successfully' };
   } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update user's payment method
+
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+    if (!currentUser) throw new Error('User not found');
+    const paymentMethod = paymentMethodSchema.parse(data);
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+    return { success: true, message: 'Payment method updated successfully' };
+  } catch {
     return { success: false, message: formatError(error) };
   }
 }
--- lib/validators.ts
+++ lib/validators.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { formatNumberWithDecimal } from './utils';
+import { PAYMENT_METHODS } from './constants';
 
 const currency = z
   .string()
@@ -30,7 +31,7 @@ export const signInFormSchema = z.object({
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
 
-// Schema for sigin up users 
+// Schema for sigin up users
 export const signUpFormSchema = z
   .object({
     name: z.string().min(3, 'Name must be at least 3 characters long'),
@@ -43,34 +44,43 @@ export const signUpFormSchema = z
     path: ['confirmPassword'],
   });
 
-  // Cart schemas
-  export const cartItemSchema = z.object({
-    productId: z.string().min(1, 'Product is required '),
-    name: z.string().min(1, 'Name is required '),
-    slug: z.string().min(1, 'Slug is required '),
-    qty: z.number().int().nonnegative('Quantity must be a positive number '),
-    image: z.string().min(1, 'Image is required'),
-    price: currency,
-  });
+// Cart schemas
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, 'Product is required '),
+  name: z.string().min(1, 'Name is required '),
+  slug: z.string().min(1, 'Slug is required '),
+  qty: z.number().int().nonnegative('Quantity must be a positive number '),
+  image: z.string().min(1, 'Image is required'),
+  price: currency,
+});
 
-  export const insertCartSchema = z
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  sessionCartId: z.string().min(1, 'Session cart is required'),
+  userId: z.string().optional().nullable(),
+});
+
+// Schema for the shipping address
+export const shippingAddressSchema = z.object({
+  fullName: z.string().min(3, 'Name must be at least 3 characters'),
+  streetAddress: z.string().min(3, 'Address is required'),
+  city: z.string().min(3, 'City is required'),
+  postalCode: z.string().min(3, 'Postal code is required'),
+  country: z.string().min(3, 'Country is required'),
+  lat: z.number().optional(),
+  lng: z.number().optional(),
+});
+
+// Schema fot the payment methods
+export const paymentMethodSchema = z
   .object({
-    items: z.array(cartItemSchema),
-    itemsPrice: currency,
-    shippingPrice: currency,
-    taxPrice: currency,
-    totalPrice: currency,
-    sessionCartId: z.string().min(1, 'Session cart is required'),
-    userId: z.string().optional().nullable(),
+    type: z.string().min(1, 'Payment method is required'),
   })
-
-  // Schema for the shipping address
-  export const shippingAddressSchema = z.object({
-    fullName: z.string().min(3, 'Name must be at least 3 characters'),
-    streetAddress: z.string().min(3, 'Address is required'),
-    city: z.string().min(3, 'City is required'),
-    postalCode: z.string().min(3, 'Postal code is required'),
-    country: z.string().min(3, 'Country is required'),
-    lat: z.number().optional(),
-    lng: z.number().optional(),
-    });
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ['type'],
+    message: 'Invalid payment method',
+  });
