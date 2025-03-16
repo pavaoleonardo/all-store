e17d7bf906b959dedc1eb03854bb1a53bf85840a
leonardo <pavaoleonardo@hotmail.com>
2025-03-16 17:53:11 +0100

    Revert "adding place order method rfct"
    This reverts commit 6fa93a6814f97aca43f4d8fa678b827480807ad1.

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -64,7 +64,7 @@ const PlaceOrderPage = async () => {
           <Card>
             <CardContent className='p-4 gap-4'>
               <h2 className='text-xl pb-4'>Payment Method</h2>
-              <p>{user.paymentMethod}</p>
+              <p>{userAddress.fullName}</p>
 
               <div className='mt-3'>
                 <Link href='/payment-method'>
